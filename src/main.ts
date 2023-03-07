6008c725288c402187095e2efe87085e07bb8df6
Erik Luu <eeluu@calpoly.edu>
2023-03-07 13:30:18 -0800

    can install dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,19 @@
 import { FileSystemAdapter, Notice, Plugin, TFile, CachedMetadata, getAllTags } from 'obsidian';
 import { PythonShell } from 'python-shell';
+const { spawn } = require('child_process');
 import { GraphGuruSettings, GraphGuruSettingTab, DefaultGuruSettings } from './settings';
-
-// const test_coordinates = [  {coords: [0.00355128, 0.14330955]},
-//                             {coords: [-0.01840003, 0.15422342]},
-//                             {coords: [-0.30591294, -0.11079613]},
-//                             {coords: [-0.27770398, -0.0548101]},
-//                             {coords: [-0.28004129, -0.11006741]},
-//                             {coords: [-0.30041763, -0.08313805]},
-//                             {coords: [-0.24007989, -0.10953094]},
-//                             {coords: [0.28609557, -0.26272716]},
-//                             {coords: [0.31823789, -0.20931468]},
-//                             {coords: [0.29171426, -0.2530871]},
-//                             {coords: [0.32597832, -0.04688666]},
-//                             {coords: [0.10558672, 0.34822129]},
-//                             {coords: [0.12127385, 0.33224248]},
-//                             {coords: [0.07495285, 0.31229199]},
-//                             {coords: [-0.03595148, -0.05764129]},
-//                             {coords: [-0.06888351, 0.00771078]}
-//                         ]
 
 export default class GraphGuruPlugin extends Plugin {
     settings: GraphGuruSettings;
-    path: string;
+    scriptsPath: string;
     pythonScriptsPath: string;
     statusBar: HTMLElement;
     guruCoordinates: object[] | unknown[] | null = null;
+    envPath: string;
 
     async onload() {
         if (this.app.vault.adapter instanceof FileSystemAdapter) {
-			this.path = this.app.vault.adapter.getBasePath();
-            // this.pythonScriptsPath = this.path + '/.obsidian/plugins/obsidian-graph-guru/src/processing/async_script.py';
-            this.pythonScriptsPath = this.path + '/.obsidian/plugins/obsidian-graph-guru/src/processing/script.py';
+			this.scriptsPath = this.app.vault.adapter.getBasePath();
 		}
 
         this.settings = Object.assign({}, DefaultGuruSettings, await this.loadData());
@@ -97,11 +79,16 @@ export default class GraphGuruPlugin extends Plugin {
 
     async sendToPython() {
         try {
-            const pyshell = new PythonShell(this.pythonScriptsPath);
+            let options = {
+                pythonPath: this.settings.pythonInterpreter,
+            }
+
+            const pyshell = new PythonShell(this.pythonScriptsPath, options);
             pyshell.send(this.app.vault.configDir); // actually send script
             pyshell.on('message', (message) => {
                 try {
-                    const output = JSON.parse(message);
+                    const output = message;
+                    console.log(output);
                 } catch (err) {
                     console.log(err);    
                 }
@@ -117,28 +104,6 @@ export default class GraphGuruPlugin extends Plugin {
         } catch (error) {
             console.log(error);
         }
-    }
-
-    async getVaultAllFiles() {
-        const files: TFile[] = this.app.vault.getFiles();
-        
-        const fileObjects = files.map(async (f) => {
-            const text: string = await this.app.vault.read(f);
-            const metadata: CachedMetadata | null = this.app.metadataCache.getFileCache(f);
-            let tags: string[] | null = null
-            
-            if (metadata != null) {
-                tags = getAllTags(metadata);    
-            }
-
-            return {
-                // metadata: metadata,
-                text: text,
-                tags: tags
-            }
-        });
-
-        return fileObjects;
     }
 
     async saveSettings() {
--- src/settings.ts
+++ src/settings.ts
@@ -1,17 +1,23 @@
-import {App, PluginSettingTab, Setting} from "obsidian";
+import {App, Notice, PluginSettingTab, Setting} from "obsidian";
+import { spawn, exec, ChildProcess } from 'child_process';
 
 import GraphGuruPlugin from "./main";
 
 export interface GraphGuruSettings {
     focus_directory: string;
+    pythonInterpreter: string;
+	setupScript: string;
 }
 
 export const DefaultGuruSettings: GraphGuruSettings= {
     focus_directory: './',
+    pythonInterpreter: 'python',
+	setupScript: '',
 } 
 
 export class GraphGuruSettingTab extends PluginSettingTab {
     plugin: GraphGuruPlugin;
+
     constructor(app: App, plugin: GraphGuruPlugin) {
         super(app, plugin);
         this.plugin = plugin;
@@ -25,15 +31,48 @@ export class GraphGuruSettingTab extends PluginSettingTab {
         containerEl.createEl('h2', {text: 'Graph Guru Settings'});
 
         new Setting(containerEl)
-            .setName('Focus Directory')
-            .setDesc('Enter the directory you wish for GraphGuru to focus on.')
-            .addText(text => text
-                .setPlaceholder('Enter your directory')
-                .setValue(this.plugin.settings.focus_directory)
-                .onChange(async (value) => {
-                    this.plugin.settings.focus_directory = value;
-                    await this.plugin.saveSettings();
-                }));
+			.setName('Python interpreter')
+			.setDesc('Path to your python interpreter, e.g. `/usr/bin/python`.')
+			.setClass('wideSettingsElement')
+			.addText(text => text
+				.setValue(this.plugin.settings.pythonInterpreter)
+				.onChange(async (value) => {
+					this.plugin.settings.pythonInterpreter = value;
+					await this.plugin.saveSettings();
+				}));
+
+        new Setting(containerEl)
+            .setName('Install python dependencies')
+            .setDesc('This will modify your environment-use at your own risk.')
+            .addButton(button => {
+                button.setButtonText('Install dependencies');
+                button.onClick(evt => {
+                    let interpreter = this.plugin.settings.pythonInterpreter;
+                    let command = `${interpreter} -u -m pip install --upgrade openai numpy obsidiantools scikit-learn tenacity`;
+                    new Notice('Installing dependencies; this may take some time...');
+                    exec(command, (error, stdout, stderr) => {
+                        if (error) {
+                            console.error(`failed to install dependencies: ${error}`);
+                            new Notice('Failed to install dependencies, view developer console for details.');
+                        } else {
+                            new Notice('Installed dependencies, view developer console for details.');
+                        }
+                        console.log(`install stdout: ${stdout}`);
+                        console.log(`install stderr: ${stderr}`);
+                    });
+                });
+            });
+
+        // new Setting(containerEl)
+        //     .setName('Focus Directory')
+        //     .setDesc('Enter the directory you wish for GraphGuru to focus on.')
+        //     .addText(text => text
+        //         .setPlaceholder('Enter your directory')
+        //         .setValue(this.plugin.settings.focus_directory)
+        //         .onChange(async (value) => {
+        //             this.plugin.settings.focus_directory = value;
+        //             await this.plugin.saveSettings();
+        //         }));
     }
 }
 
